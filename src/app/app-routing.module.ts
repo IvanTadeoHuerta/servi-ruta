d3f8854692df6c2ca61be5ee178088ca51f015f4
Ivan Tadeo Huerta <tadeohuertaivan@gmail.com>
2020-05-01 16:33:27 -0500

    Guard rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,27 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from "src/app/login/login.component";
 import { HomeComponent } from "src/app/home/home.component";
 import { MenuComponent } from "src/app/menu/menu.component";
+import { NotAuthGuard } from "src/app/core/auth/not.auth.guard";
+import { AuthGuard } from "src/app/core/auth/auth.guard";
+import { AuthService } from "src/app/core/auth/auth.service";
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent,  canActivate: [NotAuthGuard]  },
   {
-    path: 'home', component: HomeComponent, children: [
+    path: 'home', component: HomeComponent, canActivate: [AuthGuard] ,children: [
     ]
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    AuthService,
+    NotAuthGuard,
+    AuthGuard
+  ],
 })
 export class AppRoutingModule { }
